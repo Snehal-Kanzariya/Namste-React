9deec47508125223b49ae4d3214c0781ad2cce97
Snehal-Kanzariya <snehal.kanzariya02@gmail.com>
2023-01-09 10:45:26 +0530

    2 - igniting our app

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
-const heading = React.createElement("h1", {}, "Namste React");
-const heading2 = React.createElement("h2", {}, "Namste React heading 2");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {id: "heading1"}, "Namste React");
+const heading2 = React.createElement("h2", {id: "heading2"}, "Namste React heading 2");
 const container = React.createElement("div", {id: "container"}, [heading, heading2]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
